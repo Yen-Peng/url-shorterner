3ed0e668533a72195970f0bfa6f6ec6c178d7eea
Yen-Peng <teacup35@gmail.com>
2022-09-10 13:44:13 +0800

    feat: basic setup using pern stack

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+//import logo from './logo.svg';
+import React, { Fragment } from "react";
+import './App.css';
+
+import InputTodo from './components/InputTodo';
+import ListTodos from './components/ListTodos';
+
+function App() {
+  return (
+    <Fragment>
+      <div className="container">
+        <InputTodo />
+        <ListTodos />
+      </div>
+    </Fragment>
+  );
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
+}
+
+export default App;

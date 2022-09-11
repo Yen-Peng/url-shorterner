60881930a4ebac2a9fbc229e9b2b92fd463e5bfa
Yen-Peng <teacup35@gmail.com>
2022-09-12 07:05:48 +0800

    feat: change server params for deployment

--- server/db.js
+++ server/db.js
@@ -1,12 +1,19 @@
 require("dotenv").config();
 const Pool = require("pg").Pool;
 
+const isProduction = process.env.NODE_ENV === "production";
+
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+
+
 const pool = new Pool({
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  database: process.env.PG_DATABASE
+  connectionString: isProduction
+    ? process.env.DATABASE_URL // Heroku will supply us with a string called DATABASE_URL for the connectionString,
+    : connectionString,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
-module.exports = pool;
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
+
